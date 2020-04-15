d641f607800e0e345dbafe74e2280ea438f7b1a6
Diogenes Justo <diogenes.justo@gmail.com>
2020-04-14 22:08:14 -0300

    Add files via upload

---
+++ Script_base.R
@@ -0,0 +1,17 @@
+# Carga de dados
+df <- read.csv("https://raw.githubusercontent.com/diogenesjusto/FIAP/master/faturamento.csv")
+# Filtrando loja
+dfl1 <- df[df$loja=="Loja 1",]
+# analise descritiva
+plot(dfl1$GMV, type="l")
+# Separação treino teste
+treino <- dfl1[1:365,]
+teste  <- dfl1[366:396,]
+# Modelagem
+mod <- lm(GMV~X, data=treino)
+summary(mod)
+# Previsão
+prev <- predict(mod, newdata = teste )
+# Avaliação
+erro_prev <- prev-teste$GMV
+erro_med_prev <-  mean(abs(erro_prev))
