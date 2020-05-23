3d1480c2da9d0ca99173df64e9d97b43a333e498
Diogenes Justo <diogenes.justo@gmail.com>
2020-05-23 10:49:54 -0300

    Add files via upload

---
+++ SHIFT/2020/202003/predict.R
@@ -0,0 +1,13 @@
+baba <- read.csv("https://raw.githubusercontent.com/diogenesjusto/FIAP/master/SHIFT/2020/202003/baba.csv")
+
+treino <- baba[1:365,]
+teste  <- baba[366:396,]
+
+mod <- lm(venda~outmg, data=treino)
+
+p<-predict(mod, newdata=teste)
+
+df <- as.data.frame(cbind(1:31,p))
+names(df)<-c("id","venda")
+
+write.table(df, 'predict.csv', row.names = FALSE, col.names = FALSE, sep=",")
