46483888ba499b255e5cd3debb47a74517295b8e
Diogenes Justo <diogenes.justo@gmail.com>
2021-03-26 23:21:58 -0300

    atualiza

--- SHIFT/desafio/baba/predict.R
+++ SHIFT/desafio/baba/predict.R
@@ -1,4 +1,4 @@
-baba <- read.csv("https://raw.githubusercontent.com/diogenesjusto/FIAP/master/SHIFT/202007/Desafio/train_BABA.csv")
+baba <- read.csv("https://raw.githubusercontent.com/diogenesjusto/FIAP/master/SHIFT/desafio/baba/BABA.csv")
 
 treino <- baba[1:365,]
 teste  <- baba[366:396,]
@@ -11,5 +11,3 @@ df <- as.data.frame(cbind(1:31,p))
 names(df)<-c("id","venda")
 
 write.table(df, 'predict.csv', row.names = FALSE, col.names = FALSE, sep=",")
-
-baba$data <- as.Date(as.character(baba$date), "%Y%m%d")
